5f0f4f8c8a016b35f9d0fff05738d9784c5bc3f6
Iswar <rick731202@gmail.com>
2022-05-19 10:27:55 +0530

    besic file upload is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
+import { storage } from "./firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
+import { useState } from 'react';
 
 function App() {
+
+  const [bookFileData, setBookFileData] = useState(null)
+  const [bookFileUrl, setBookFileUrl] = useState(null)
+
+  const uploadBookFile = async () =>{
+    
+    if (bookFileData == null) return;
+
+    const bookFileRef = ref(storage, `bookFiles/${bookFileData.name + v4()}`);
+    uploadBytes(bookFileRef, bookFileData)
+      .then(
+        (snapshot)=>{
+          getDownloadURL(snapshot.ref)
+          .then((url)=>{
+            setBookFileUrl((prev)=> [...prev, url])
+          })
+        }
+      ).catch(err => console.log(err.message))
+
+  }
+
+  console.log(bookFileUrl);
+
   return (
-    <div className="App">
-      <form>
+    <div>
+      <div >
         <label htmlFor="bookFile">Select your book</label>
-        <input type="file" id="bookFile" />
-      </form>
+        <br />
+        <input type="file" id="bookFile" onChange={(e)=>{
+          setBookFileData(e.target.files[0])
+        }} />
+        <br />
+        <button onClick={uploadBookFile}>Upload</button>
+      </div>
     </div>
   );
 }
